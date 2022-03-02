d11ed5ddcbc3eeb55e77535d7284580a77a2e666
Manzaraz <magnusmanz@gmail.com>
2022-03-02 17:23:46 -0300

    Pages: Home / Componentes: Navbar, Announcement

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// import Announcement from "../components/Announcement";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Announcement />
+      <Navbar />
+    </>
+  );
+};
+
+export default Home;
