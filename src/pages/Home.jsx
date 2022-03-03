630f11c45ad0d7d1efb79864280b1653f3946bf8
Manzaraz <magnusmanz@gmail.com>
2022-03-03 00:31:55 -0300

    create a pure react slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 // import Announcement from "../components/Announcement";
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
+import Slide from "../components/Slider";
 
 const Home = () => {
   return (
     <>
       <Announcement />
       <Navbar />
+      <Slide />
     </>
   );
 };
