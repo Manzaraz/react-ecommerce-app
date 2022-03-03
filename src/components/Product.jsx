780ec47129d0672affa2331c9bc93087ec153be4
Manzaraz <magnusmanz@gmail.com>
2022-03-03 10:56:48 -0300

    Pause project

---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Product = ({ item }) => {
+  return <Container>Product</Container>;
+};
+
+export default Product;
