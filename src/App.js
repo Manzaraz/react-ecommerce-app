f0836c63a6cbab69e9097c5284f9feb9e815866f
Manzaraz <magnusmanz@gmail.com>
2022-03-06 20:27:35 -0300

    react-router implementation

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
+
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <div className="App">
-      {/* <Home /> */}
-      {/* <ProductList /> */}
-      {/* <Product /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      <Cart />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 }
 
